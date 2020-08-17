00b0839e067c372d9f049ccfb6a5a0a6da9676c9
zpl <zplalipay@gmail.com>
2020-08-17 22:18:42 +0800

    #创建application

--- app/src/main/java/com/zpl/wanandroid/App.kt
+++ app/src/main/java/com/zpl/wanandroid/App.kt
@@ -1,6 +1,36 @@
 package com.zpl.wanandroid
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import com.scwang.smartrefresh.header.MaterialHeader
+import com.scwang.smartrefresh.layout.SmartRefreshLayout
+import com.scwang.smartrefresh.layout.api.RefreshLayout
+import com.scwang.smartrefresh.layout.footer.BallPulseFooter
 
-class App:Application() {
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        baseApplication = this
+        MultiDex.install(this)
+
+        initSmartHead()
+    }
+
+    private fun initSmartHead() {
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context: Context, _: RefreshLayout ->
+            MaterialHeader(context)
+        }
+        SmartRefreshLayout.setDefaultRefreshFooterCreator { context: Context, _: RefreshLayout ->
+            BallPulseFooter(context)
+        }
+    }
+
+    companion object {
+        private lateinit var baseApplication: Application
+
+        fun getContext(): Context {
+            return baseApplication
+        }
+    }
 }
